7e4524086815136c48d5f55883e1c93b708ac338
moises <ldcastil@bancoagricola.com.sv>
2021-01-08 21:38:47 -0600

    Se crearon las paginas Principales
    paginas principales mas la instalación del  paqetes de navegacion

---
+++ app/screens/Favorites.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {View , Text} from "react-native";
+
+export default function Favorites()
+{
+    return (
+        <view>
+            <text>Favorites...</text>
+        </view>
+    )
+}
